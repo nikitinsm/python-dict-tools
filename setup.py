a592dde2b4389ff3d3a295411fa97ec3d157c968
sergey <nikitinsm@gmail.com>
2015-09-08 11:09:27 +0300

    ADD: setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup\
+    ( name='dict_tools'
+    , version='1.0'
+    , description='Tools for working with big and npredictable data structures'
+    , author='Sergey Nikitin'
+    , author_email='nikitinsm@gmail.com'
+    , url='https://github.com/nikitinsm/python-dict-tools'
+    , packages = find_packages('src')
+    , package_dir = {'': 'src'}
+    , include_package_data = True
+    )
