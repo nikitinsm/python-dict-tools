6b5c66a1a2eb84ed89be1aaaa2cdc552f9f9d0a9
sergey <nikitinsm@gmail.com>
2015-09-08 10:39:11 +0300

    INITIAL

---
+++ src/dict_tools/__init__.py
@@ -0,0 +1,63 @@
+import collections
+
+
+def get(d, *path, **kwargs):
+    default = kwargs.get('default', None)
+    strict = kwargs.get('strict', False)
+
+    for member in path:
+        # try index as is
+        try:
+            d = d[member]
+            continue
+        except (TypeError, KeyError, AttributeError) as e:
+            if strict:
+                raise KeyError('%s undefined' % '.'.join(map(repr, path)))
+
+        # try index as str
+        try:
+            d = d[str(member)]
+            continue
+        except (TypeError, KeyError, AttributeError) as e:
+            if strict:
+                raise KeyError('%s undefined' % '.'.join(map(repr, path)))
+
+        # try attribute
+        try:
+            d = getattr(d, member)
+            continue
+        except (AttributeError, TypeError) as e:
+            if strict:
+                raise KeyError('%s undefined' % '.'.join(map(repr, path)))
+
+        # try int index
+        try:
+            d = d[int(member)]
+            continue
+        except (TypeError, KeyError, ValueError, AttributeError) as e:
+            if strict:
+                raise KeyError('%s undefined' % '.'.join(map(repr, path)))
+
+        d = default
+        break
+    return d
+
+
+def merge(d1, d2):
+    for k, v in d2.iteritems():
+        if isinstance(v, collections.Mapping):
+            r = merge(d1.get(k, {}), v)
+            d1[k] = r
+        else:
+            d1[k] = d2[k]
+    return d1
+
+
+def copy(d):
+    out = d.copy()
+    for k, v in d.iteritems():
+        if isinstance(v, dict):
+            out[k] = copy(v)
+        elif isinstance(v, list):
+            out[k] = v[:]
+    return out
