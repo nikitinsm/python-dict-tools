7a8602672d89de6043e35bfadc84034a44f33e4d
Nikitin S.M <nikitinsm@gmail.com>
2016-07-26 08:26:53 +0300

    CLEANUP

--- watch_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# pip install watchdog to get ability of watchmedo
-# pip install nose to get ability of nosetests
-watchmedo shell-command --recursive -p "*.py" -c "nosetests --nocapture" --interval=30 --drop ./
